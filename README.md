978b2dc2ade1d1bc4518ea3afa410b324ac1ad77
AtlantoCZ <54130993+AtlantoCZ@users.noreply.github.com>
2023-11-12 19:20:19 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,77 @@
+# Boards
+
+This command-line tool downloads board games as PGN (chess), KIF (shogi), PDN (draughts), SGF (go) or TXT (mill) files from these online websites:
+
+- **Chess**
+	- 2700chess.com
+	- 365chess.com
+	- Chess.com
+	- Chess.org
+	- Chess24.com
+	- ChessArena.com
+	- ChessBase.com
+	- ChessBomb.com
+	- ChessGames.com
+	- ChessKing.com
+	- ChessPastebin.com
+	- ChessPro.ru
+	- ChessPuzzle.net
+	- Chess-Samara.ru
+	- ChessTempo.com
+	- ChessVariants.training
+	- Echecs-Online.eu
+	- Europe-Echecs.com
+	- Ficgs.com
+	- FicsGames.org
+	- GameKnot.com
+	- GChess.com
+	- GreenChess.net
+	- Iccf.com
+	- IdeaChess.com
+	- Immortal.game
+	- Lichess.org
+	- Lishogi.org
+	- Listudy.org
+	- LiveChess24.com
+	- LiveChess.aunz.net
+	- LiveChessCloud.com
+	- PlayOK.com
+	- PlayStrategy.org
+	- Pychess.org
+	- RedHotPawn.com
+	- Schach-Spielen.eu
+	- SchachArena.de
+	- SchemingMind.com
+	- TheChessWorld.com
+- **Draughts**
+	- Lidraughts.org
+	- PlayOK.com
+	- PlayStrategy.org
+- **Go**
+	- DragonGoServer.net
+	- GoKGS.com
+	- Ingo-web.com
+	- Online-go.com
+	- PlayOK.com
+- **Mill**
+	- PlayOK.com
+- **Others (othello, Asian boards...)**
+	- PlayStrategy.org
+	- Pychess.org
+
+
+## Install
+
+- Install Python 3
+- Install the dependencies: `pip install chess`
+- Download your games: `python boards.py download https://your-board.tld/game/7E5F59av`
+
+
+## Licence
+
+```
+Copyright (C) 2019-2020 Pychess
+Copyright (C) 2021-2023 ecrucru
+https://github.com/ecrucru/boards
+GPL version 3
+```
---
+++ boards.py
@@ -0,0 +1,337 @@
+# Copyright (C) 2019-2020 Pychess
+# Copyright (C) 2021-2022 ecrucru
+# https://github.com/ecrucru/boards
+# GPL version 3
+
+
+# Libraries
+from typing import Optional, List
+import argparse
+import asyncio
+import logging
+import sys
+import re
+from urllib.parse import urlparse
+from random import choice
+
+from lib.const import BOARDS_DESC, METHODS_DESC
+from lib.bp_interface import InternetGameInterface
+
+# Popular chess
+from lib.bp_lichess import InternetGameLichess
+from lib.bp_chesscom import InternetGameChessCom
+from lib.bp_chess24 import InternetGameChess24
+# Chess
+from lib.bp_2700chess import InternetGame2700chess
+from lib.bp_365chess import InternetGame365chess
+from lib.bp_chessarena import InternetGameChessarena
+from lib.bp_chessbase import InternetGameChessbase
+from lib.bp_chessbomb import InternetGameChessbomb
+# TODO ChessDuo.com
+from lib.bp_chessgames import InternetGameChessgames
+from lib.bp_chessking import InternetGameChessking
+from lib.bp_chessorg import InternetGameChessOrg
+from lib.bp_chesspastebin import InternetGameChesspastebin
+from lib.bp_chesspro import InternetGameChesspro
+from lib.bp_chesspuzzle import InternetGameChesspuzzle
+from lib.bp_chesssamara import InternetGameChesssamara
+from lib.bp_chesstempo import InternetGameChesstempo
+from lib.bp_chessvariants import InternetGameChessvariants
+from lib.bp_echecsonline import InternetGameEchecsonline
+from lib.bp_europeechecs import InternetGameEuropeechecs
+from lib.bp_ficgs import InternetGameFicgs
+from lib.bp_ficsgames import InternetGameFicsgames
+from lib.bp_gameknot import InternetGameGameknot
+from lib.bp_gchess import InternetGameGchess
+from lib.bp_greenchess import InternetGameGreenchess
+from lib.bp_iccf import InternetGameIccf
+from lib.bp_immortal import InternetGameImmortal
+from lib.bp_ideachess import InternetGameIdeachess
+from lib.bp_lishogi import InternetGameLishogi
+from lib.bp_listudy import InternetGameListudy
+from lib.bp_livechess24 import InternetGameLivechess24
+from lib.bp_livechessaunz import InternetGameLivechessAunz
+from lib.bp_livechesscloud import InternetGameLivechesscloud
+from lib.bp_playok import InternetGamePlayokChess
+from lib.bp_playstrategy import InternetGamePlaystrategy
+from lib.bp_pychess import InternetGamePychess
+from lib.bp_redhotpawn import InternetGameRedhotpawn
+from lib.bp_schacharena import InternetGameSchacharena
+from lib.bp_schachspielen import InternetGameSchachspielen
+from lib.bp_schemingmind import InternetGameSchemingmind
+from lib.bp_thechessworld import InternetGameThechessworld
+# Draughts
+from lib.bp_lidraughts import InternetGameLidraughts
+from lib.bp_playok import InternetGamePlayokDraughts8, InternetGamePlayokDraughts10
+# Go
+from lib.bp_dragongoserver import InternetGameDragongoserver
+from lib.bp_gokgs import InternetGameGokgs
+from lib.bp_ingoweb import InternetGameIngoweb
+from lib.bp_onlinego import InternetGameOnlinego
+from lib.bp_playok import InternetGamePlayokGo, InternetGamePlayokGomoku
+# Mill
+from lib.bp_playok import InternetGamePlayokMill
+# Lowest priority
+from lib.bp_generic_chess import InternetGameGenericChess
+# Import the Tkinter library
+import tkinter as tk
+from tkinter import ttk
+from tkinter import scrolledtext
+from tkinter import filedialog  # Import the file dialog module
+import threading
+
+board_providers: List[InternetGameInterface] = []
+
+
+# Retrieve a game from a URL
+async def download(url: str) -> Optional[str]:
+    # Recognize the most popular identifiers
+    if url in [None, '']:
+        return None
+    if re.compile(r'^[a-z0-9-]{8}$', re.IGNORECASE).match(url) is not None:
+        url = 'https://lichess.org/' + url
+    elif re.compile(r'^[a-z0-9]{5}$', re.IGNORECASE).match(url) is not None:
+        url = 'https://lichess.org/training/' + url
+    elif url.isdigit():
+        url = 'https://www.chess.com/live/game/' + url
+
+    # Check the format
+    p = urlparse(url.strip())
+    if '' in [p.scheme, p.netloc]:
+        return None
+    logging.debug('URL to retrieve: %s', url)
+
+    # Call the board providers
+    for bp in board_providers:
+        if not bp.is_enabled():
+            continue
+        bp.reset()
+        if bp.assign_game(url):
+            # Download
+            logging.debug('Responding board provider: %s', bp.get_description())
+            try:
+                if bp.is_async():
+                    pgn = await bp.download_game()
+                else:
+                    pgn = bp.download_game()
+                pgn = bp.sanitize(pgn)
+            except Exception as e:
+                pgn = None
+                logging.debug(str(e))
+
+            # Check
+            if pgn is None:
+                logging.debug('Download failed')
+            else:
+                logging.debug('Successful download')
+                return pgn
+    return None
+
+# Create a simple Tkinter GUI
+class ChessDownloaderUI:
+    def __init__(self, root):
+        self.root = root
+        root.title("Chess Downloader")
+
+        # URL entry
+        self.url_label = ttk.Label(root, text="Enter URL:")
+        self.url_label.grid(column=0, row=0)
+
+        self.url_entry = ttk.Entry(root, width=50)
+        self.url_entry.grid(column=1, row=0)
+
+        # Download button
+        self.download_button = ttk.Button(root, text="Download", command=self.start_download_thread)
+        self.download_button.grid(column=1, row=1)
+
+        # Result display
+        self.result_label = ttk.Label(root, text="Downloaded Game:")
+        self.result_label.grid(column=0, row=2)
+
+        self.result_text = scrolledtext.ScrolledText(root, width=50, height=10, wrap=tk.WORD)
+        self.result_text.grid(column=1, row=2, rowspan=2)
+
+        # Save button
+        self.save_button = ttk.Button(root, text="Save to PGN", command=self.save_to_pgn)
+        self.save_button.grid(column=1, row=4)
+
+        # Initialize downloaded_data attribute
+        self.downloaded_data = None
+
+    def start_download_thread(self):
+        # Start a new thread to run the download function
+        download_thread = threading.Thread(target=self.download)
+        download_thread.start()
+
+    def download(self):
+        # SSL (unverified) checkbox
+        unverified_ssl = False  # You may want to add a checkbox in the UI for this
+        if unverified_ssl:
+            import ssl
+            ssl._create_default_https_context = ssl._create_unverified_context
+
+        # Download
+        url = self.url_entry.get()
+        if url:
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+
+            try:
+                self.downloaded_data = loop.run_until_complete(self.download_async(url))
+            finally:
+                loop.close()
+
+        else:
+            self.result_text.delete(1.0, tk.END)
+            self.result_text.insert(tk.END, "Please enter a valid URL.")
+
+    async def download_async(self, url):
+        # Clear the result_text before downloading new data
+        self.result_text.delete(1.0, tk.END)
+
+        data = await download(url)
+
+        # Display result in the UI
+        if data is not None:
+            self.result_text.insert(tk.END, data)
+            self.show_save_dialog(data)
+        else:
+            self.result_text.insert(tk.END, "No game found.")
+
+    def show_save_dialog(self, data):
+        # Ask the user for the destination folder and file name
+        file_path = filedialog.asksaveasfilename(defaultextension=".pgn",
+                                                    filetypes=[("PGN Files", "*.pgn")],
+                                                    title="Save as")
+
+        if file_path:
+            # Save the downloaded data to the selected file
+            try:
+                with open(file_path, "w", encoding="utf-8") as file:
+                    file.write(data)
+                self.result_text.insert(tk.END, f"\n\nData saved to {file_path}")
+            except Exception as e:
+                self.result_text.insert(tk.END, f"\n\nError saving data: {str(e)}")
+
+    def save_to_pgn(self):
+        if self.downloaded_data is not None:
+            self.show_save_dialog(self.downloaded_data)
+        else:
+            self.result_text.insert(tk.END, "\n\nNo data to save.")
+
+
+            
+# Start of the program
+async def main() -> None:
+    # Load the board providers from the imported classes
+    for cls in InternetGameInterface.__subclasses__():
+        if len(cls.__subclasses__()) == 0:
+            board_providers.append(cls())
+        else:
+            for cls2 in cls.__subclasses__():
+                board_providers.append(cls2())
+
+    # General Unicode
+    try:
+        sys.stdout.reconfigure(encoding='utf-8')
+    except Exception:
+        pass
+
+    # Command line
+    cmdline = argparse.ArgumentParser(prog='python boards.py', description='Boards is a download helper for the online board games (chess, draughts, go, mill)')
+    subparser = cmdline.add_subparsers(dest='command')
+
+    subparser.add_parser('show', help='Show the supported board providers')
+
+    group = subparser.add_parser('download', help='Download a game')
+    group.add_argument('url', default='', help='URL of the board game')
+    group.add_argument('--unverified-ssl', action='store_true', help='Use an unverified SSL context to avoid some errors with SSL')
+
+    subparser.add_parser('test', help='Run the quality test')
+
+    # Execute
+    parser = cmdline.parse_args()
+    if parser.command == 'show':
+        plist = []
+        for bp in board_providers:
+            if bp.is_enabled():
+                site, board, method = bp.get_identity()
+                plist.append('%s - %s [%s]' % (BOARDS_DESC[board], site, METHODS_DESC[method]))
+        plist.sort()
+        for bp in plist:
+            print(bp)
+
+    elif parser.command == 'download':
+        # SSL
+        if parser.unverified_ssl:
+            import ssl
+            ssl._create_default_https_context = ssl._create_unverified_context
+
+        # Download
+        data = await download(parser.url)
+        if data is not None:
+            print(data)
+        else:
+            logging.error('No game found.')
+
+    elif parser.command == 'test':
+        logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+        errors = 0
+        for bp in board_providers:
+            # Check
+            if bp is None:
+                continue
+            logging.info('================')
+            logging.info('Site: %s', bp.get_description())
+            links = bp.get_test_links()
+            if len(links) == 0:
+                logging.info('No available test link')
+                continue
+            if not bp.is_enabled():
+                logging.info('Disabled module')
+                continue
+
+            # Pick one link only to not overload the remote server
+            url, expected = choice(links)
+            logging.info('Target link: %s', url)
+            logging.info('Expecting data: %s', expected)
+
+            # Download link
+            bp.reset()
+            if not bp.assign_game(url):
+                data = None
+            else:
+                try:
+                    if bp.is_async():
+                        data = await bp.download_game()
+                    else:
+                        data = bp.download_game()
+                    data = bp.sanitize(data)
+                except Exception as e:
+                    logging.debug(str(e))
+                    data = None
+
+            # Result
+            ok = data is not None
+            logging.info('Fetched data: %s', ok)
+            if ok:
+                logging.info(data)
+            if ok != expected:
+                logging.error('Test in error')
+                errors += 1
+
+        if errors > 0:
+            logging.error('The unit test failed %d times', errors)
+        else:
+            logging.info('The unit test is successful')
+
+    else:
+        cmdline.print_help()
+
+# Create and run the Tkinter GUI
+    root = tk.Tk()
+    app = ChessDownloaderUI(root)
+    root.mainloop()
+
+if __name__ == '__main__':
+    asyncio.run(main())
